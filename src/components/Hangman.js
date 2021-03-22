0743534c9bfb4b53cece6a7d5ff3983dfd6f7008
Silver <silverdev@protonmail.com>
2021-03-22 09:58:38 +0200

    Added basic game functionality.

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -12,22 +12,34 @@ import img6 from './images/step6.jpg'
 
 class Hangman extends Component {
     static defaultProps = {
-        numMistakes: 0,
+        maxMistakeNum: 7,
         images: [img0, img1, img2, img3, img4, img5, img6]
     }
 
     constructor(props) {
         super(props)
         this.state = {
-            mistakes: 0,
-            //guessed: new Set([]),
+            mistakeNum: 0,
+            guessed: new Set([]),
             answer: randomWord()
         }
     }
+    guessedWord() {
+        return (this.state.answer.split("").map(letter => (this.state.guessed.has(letter) ? letter : " _ ")))
+        // If the letter has been guessed correctly, show it. Otherwise, show an underscore.
+    }
     render() {
+        const gameOver = this.state.mistakeNum >= this.props.maxMistakeNum;
         return (
             <div className="Hangman container">
                 <h1 className="text-center">Hangman</h1>
+                <div className="float-right">Wrong Guesses: {this.state.mistakeNum} / {this.props.maxMistakeNum}</div>
+                <div className="text-center"><img src={this.props.images[this.state.mistakeNum]} alt="" /></div>
+                {/* Current out of total number of mistakes. */}
+                <div className="text-center">
+                    <p>Mystery Word:</p>
+                    <p>{!gameOver ? this.guessedWord() : this.state.answer}</p>
+                </div>
             </div>
         )
     }
