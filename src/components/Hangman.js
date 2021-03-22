da18c4b4b7c23edb3958d9df742c8dbdcacb1bd6
Silver <silverdev@protonmail.com>
2021-03-22 07:47:23 +0200

    Added basic files.

---
+++ src/components/Hangman.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './hangman.css'
+import { randomWord } from './Words.js'
+
+class Hangman extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hangman</h1>
+            </div>
+        )
+    }
+}
+
+export default Hangman;
